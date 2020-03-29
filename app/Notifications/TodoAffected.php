1d1545b6a6102f37605f870d4be0a7204b282883
ITANéA <frederic.blanc@itanea.fr>
2020-03-29 12:47:10 +0200

    Update mail message

--- app/Notifications/TodoAffected.php
+++ app/Notifications/TodoAffected.php
@@ -44,7 +44,7 @@ class TodoAffected extends Notification
     {
         return (new MailMessage)
                     ->from('noreply@laravel-todo.itanea.fr', 'Laravel Todo')
-                    ->line('La todo #' . $this->todo->id . ' vient de vous être affecté par ' . $this->todo->todoAffectedBy->name . '.')
+                    ->line('La todo #' . $this->todo->id . ' vient de t\'être affecté par ' . $this->todo->todoAffectedBy->name . '.')
                     ->action('Voir toutes mes todos', url('/todos'))
                     ->line('Merci d\'utiliser notre application !');
     }
